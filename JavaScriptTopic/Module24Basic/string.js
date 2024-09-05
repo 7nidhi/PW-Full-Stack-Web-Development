8b0c9d80ab4a196d74d99f3782dbd1aea9144588
7nidhi <nidhi151@gmail.com>
2024-09-05 13:06:16 +0530

    backtick string writing

--- JavaScriptTopic/Module24Basic/string.js
+++ JavaScriptTopic/Module24Basic/string.js
@@ -1,4 +1,10 @@
 let singleQuote='I am Nidhi,';
 let doubleQuote="I am full stack dveloper,";
-let backtickString=`I have 15LPA's in Banglore`;
+let backtickString="I have 15LPA's in Banglore"; 
 console.log(singleQuote,doubleQuote,backtickString);
+
+//Backtick isised for multiline text writting.//
+let multilineString=`I am Nidhi
+from Banaras
+I live in Pune`;
+console.log(multilineString);
