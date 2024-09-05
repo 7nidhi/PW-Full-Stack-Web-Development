9c26071f10f31e215cc044fa29f94c6a97421b71
7nidhi <nidhi151@gmail.com>
2024-09-06 00:15:58 +0530

    Adding Arithmtic Operators

---
+++ JavaScriptTopic/Module28Basic_Operators/arithmeticOp.js
@@ -0,0 +1,24 @@
+/*arithmetic operators */
+
+//Addition
+let num1=7;
+let num2=15;
+console.log(num1+num2+10);
+
+//Subtraction
+num1=10;
+num2=5;
+console.log(num1-num2);
+
+//Multiplication
+console.log(num1*num2);
+
+//Division
+console.log(num1/num2);
+
+//Exponent/power
+console.log(2**3);
+
+//unary operator ++a,a++,a--,--a
+
+
