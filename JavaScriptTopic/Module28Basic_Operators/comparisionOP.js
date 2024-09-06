e20cc2514387fa70c82bd9c0a929acf2fa233b17
7nidhi <nidhi151@gmail.com>
2024-09-06 16:45:05 +0530

    Comparision Operator

--- JavaScriptTopic/Module28Basic_Operators/comparisionOP.js
+++ JavaScriptTopic/Module28Basic_Operators/comparisionOP.js
@@ -6,3 +6,6 @@ console.log(a<=b);
 console.log(a>=b);
 
 //equality operators (== , change the type of operand,===, no type conversion)
+console.log(5== "5");
+console.log(5==="5");
+console.log(5===5);
