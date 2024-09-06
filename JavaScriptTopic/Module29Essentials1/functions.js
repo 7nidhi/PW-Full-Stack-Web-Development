f15a14e4322b8b670c1091d180252c1dd0918c52
7nidhi <nidhi151@gmail.com>
2024-09-07 03:32:45 +0530

    Functions

---
+++ JavaScriptTopic/Module29Essentials1/functions.js
@@ -0,0 +1,16 @@
+function isEvenOrOdd(x){ //x is parameter
+    if(x%2==0){
+        console.log("Even");
+    }
+    else{
+        console.log("Odd");
+    }
+}
+
+isEvenOrOdd(11);  //this is argument, actual input
+isEvenOrOdd(4);
+
+function multiply(a,b){
+    console.log(a*b);
+}
+ multiply(10,5);
