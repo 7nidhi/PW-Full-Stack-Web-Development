31280bd00331a7364dad0af10be2955cd444b0ae
7nidhi <nidhi151@gmail.com>
2024-09-07 03:07:54 +0530

    Loops in JS

---
+++ JavaScriptTopic/Module29Essentials1/loops.js
@@ -0,0 +1,19 @@
+//while loop
+let x=1;
+while(x<=10){
+    console.log(x);
+    x+=1;
+}
+
+// for loops 
+
+for(let y=0;y<=10;y++){
+    console.log(y);
+}
+
+//do whole loop
+let i=1;
+do{
+    console.log(i);
+    i+=1;
+}while(i<=10);
