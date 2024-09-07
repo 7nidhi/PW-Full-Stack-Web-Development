f2f12c981c4f3db8950af7f791b79c314ffc1dd9
7nidhi <nidhi151@gmail.com>
2024-09-08 03:52:54 +0530

    String in JS

---
+++ JavaScriptTopic/Module30Essentials2/strings.js
@@ -0,0 +1,18 @@
+const str1="nidhi \n pal";
+const str2=" Nidhi \t pal";
+const str3=` Nidhi pal is \`SDE 2 developer \` `;
+
+console.log(str1);
+console.log(str2);
+console.log(str3);
+
+const name="Nidhi";
+console.log(name.concat("-","Singh"));
+
+const s1="nidhi";
+const s2="pal";
+const s3="Banaras";
+
+let result=" ";
+result=s1+s2+s3;
+console.log(result);
