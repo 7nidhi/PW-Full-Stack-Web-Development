21aa9dedb04c3a7f20da2b381b0efed023ea761c
7nidhi <nidhi151@gmail.com>
2024-09-07 17:10:13 +0530

    Details On Function

---
+++ JavaScriptTopic/Module29Essentials1/detailsOnFunction.js
@@ -0,0 +1,25 @@
+function isEvenOdd(x){
+    if(x%2==0){
+        return "even"
+    }
+    else{
+        return "Odd"
+        }
+}
+
+let ans=isEvenOdd(10);
+console.log(ans);
+
+//2 parameter
+
+function add(x, y=5){  //we can define default parameter if we do not pass manualy any parameter in argument
+    return x+y;
+}
+
+console.log(add(10));
+
+//
+function fun(...values){
+    console.log(values);
+}
+fun(10,20,30,30,40,50);
