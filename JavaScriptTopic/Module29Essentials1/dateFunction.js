c3099aa114a2ecfec9c3f155f8fa29862aea22b1
7nidhi <nidhi151@gmail.com>
2024-09-07 17:52:03 +0530

    Math Funtions

---
+++ JavaScriptTopic/Module29Essentials1/dateFunction.js
@@ -0,0 +1,15 @@
+const d=new Date();
+console.log(d);
+console.log(d.getDate());
+console.log(d.getDay());
+console.log(d.getFullYear());
+console.log(d.getMonth());
+console.log(d.getMinutes());
+console.log(d.getSeconds());
+console.log(d.getHours());
+
+//string interpolation
+
+let x=10;
+let ans=`value of is x is ${x}`  //templeted string
+console.log(ans); 
---
+++ JavaScriptTopic/Module29Essentials1/mathFunctions.js
@@ -0,0 +1,9 @@
+console.log(Math.abs(-5));
+console.log(Math.floor(7.5));
+console.log(Math.ceil(7.7));
+console.log(Math.round(7.4));
+console.log(Math.min(10,-10,5,-5,15));
+console.log(Math.max(10,-10,5,-5,15));
+console.log(Math.sqrt(100));
+console.log(Math.random());
+console.log(25,10,1997,"Nidhi","Full Stack Developer");
