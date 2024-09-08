97c6c13b15cc3c6fa8e52727b6a904f6e0849319
7nidhi <nidhi151@gmail.com>
2024-09-08 17:22:01 +0530

    Object In JS

--- JavaScriptTopic/Module31AdvanceJS1/objects.js
+++ JavaScriptTopic/Module31AdvanceJS1/objects.js
@@ -1,3 +1,29 @@
+let obj={id:1, name:"Nidhi", age:26, city:"Banaras"};
+console.log(obj);
+let emp= new Object();  //object create
+emp.id=10;
+emp.name="Ram";
+emp.age=26;
+console.log(emp);
+function Emp(i,n,s){
+    this.id=i;
+    this.name=n;
+    this.salary=s;
+}
+
+const e=new Emp(103, "Ammy", 150000);
+console.log(e);
+console.log(emp.id);   //fetching data 
+console.log(emp['name']);  //fetching data
+
+emp.salary=150000; //updating keys and values
+console.log(emp);
+emp['name']="Radhey"; //updatng the value
+
+console.log(emp);
+
+delete emp.id;  //this will delete the keys and value
+console.log(emp);
 
 
 let product={
