2088ef120d1feb952dfcea9ad5eabe318640447e
7nidhi <nidhi151@gmail.com>
2024-09-08 16:07:55 +0530

    Object Introduction

---
+++ JavaScriptTopic/advanceJS/objects.js
@@ -0,0 +1,11 @@
+let product={
+    name:"IPhone 15max",
+    Company:"Apple",
+    price:150000,
+    warannty:2,
+    color:"white",
+}
+
+Object.keys(product); //fetch all keys of object
+Object.values(product); //fetch all value of object
+Object.entries(product); //getch all keys values pair
