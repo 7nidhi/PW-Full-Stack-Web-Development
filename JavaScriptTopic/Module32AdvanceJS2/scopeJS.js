2a03543f21f2f4ff62c6590145daa7bc3a3035b3
7nidhi <nidhi151@gmail.com>
2024-09-10 00:29:07 +0530

    Scopes in JS

---
+++ JavaScriptTopic/Module32AdvanceJS2/scopeJS.js
@@ -0,0 +1,3 @@
+//we use concept of scope to figure out where variable or function is accessible/visible.
+//4 pillar of JavaSript
+//1:Scopes and Closure , 2:Coercion and types, 3:Objects and Classes, 4: Async Programming
