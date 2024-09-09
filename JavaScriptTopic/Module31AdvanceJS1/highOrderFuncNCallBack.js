746074d6ecfd3c9e05172bb500151de0f4197ea3
7nidhi <nidhi151@gmail.com>
2024-09-09 10:41:37 +0530

    Higher order and call back function

--- JavaScriptTopic/Module31AdvanceJS1/highOrderFuncNCallBack.js
+++ JavaScriptTopic/Module31AdvanceJS1/highOrderFuncNCallBack.js
@@ -8,3 +8,12 @@ h(10, function (){
     console.log("done with call back");
 
 })
+
+//Synchronous code- code will run line by line
+console.log("Start");
+setTimeout(function(){
+    console.log("timer done")
+} ,3000);
+console.log("End");
+
+//Problem with Callback function-1:Callback hell-callback into callback into callback,2:Inversion control-give control to other function 
