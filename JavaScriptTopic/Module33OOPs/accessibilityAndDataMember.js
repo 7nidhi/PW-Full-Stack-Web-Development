4684f0c08447b139cf377b01538beb2be6389d5e
7nidhi <nidhi151@gmail.com>
2024-09-20 13:15:15 +0530

    Accessibility in JS

--- JavaScriptTopic/Module33OOPs/accessibilityAndDataMember.js
+++ JavaScriptTopic/Module33OOPs/accessibilityAndDataMember.js
@@ -18,8 +18,37 @@ class product{
     displayProduct(){
         console.log(this.#name,this.#price,this.#despription);
     }
+    getPrice(){    //getter method to get  new value
+        return this.#price;
+    }   
+    setPrice(p){       //setter method to set or update new value
+        if(p>0){
+            this.#price=p;
+        }
+        else
+        {
+            console.log("Invalid Price");
+        }
+    } 
+    
 }
 /*Now to make sure that users are able to get and set the values of our data members, we write getter an setter method.
 these getter and setter methods will helps us to write validation logic so that no one can allocate random values
 to our data members.
 */
+/* there is an alternative way to define a getter and setter method in jS. we can use the get and set keyowrd to handle this.*/
+class Prod{
+    #des;
+    get des(){
+        return this.#des;
+    }
+    set des(d){
+        if(d.length===0){
+            console.log("invalid des");
+            return;
+        }
+        this.#des=d;
+
+    }
+
+}
