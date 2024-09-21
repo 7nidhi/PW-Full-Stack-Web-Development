f5f2094ea41c74cbdfe688db28620a4f3eb353ae
7nidhi <nidhi151@gmail.com>
2024-09-21 17:23:30 +0530

    Accessibility and data member

--- JavaScriptTopic/Module33OOPs/accessibilityAndDataMember.js
+++ JavaScriptTopic/Module33OOPs/accessibilityAndDataMember.js
@@ -52,3 +52,38 @@ class Prod{
     }
 
 }
+// How to call them
+const iphone= new Prod();
+iphone.des="Something";
+console.log(iphone.des);
+
+//iphone.des="Something" will help us to access the setter and pass something as argument to the setter function.
+// iphone.des will help us to call the getter method defined.
+
+/*   Class as wrappers on Fucntion
+>In js every class we write is  wrapper over funtion, in older version of jS 'class' keyowrd does not exist.
+hence due to  do the blue print we use have only fucntions.
+
+fucntion product(n,p,d){
+    this.name=n;
+    this.price=p;
+    this.description=d;
+
+    this.displayProduct=fucntion(){
+    console.log("Name",this.name,"Price",this.price,"description",this.description)
+    }
+}
+>Now the above product fucntion acts as Fucton Constuctor, here it takes a few parameters and uses 'this' keyowrd
+to assign them.To call this constructor we do:
+
+let iphone=new Product("Iphone16",150000,"Apple Iphone");
+
+> Now here we are calling fucntion constructor of product with new keyword.and new keyowrd will do 4 things-
+1-create an empty object.
+2- assign 'this' keyword in the fucntion constructor to the new empty object and call the constructor.
+3-Do prototyping.
+4-If the constructor returns a new object manually, reuturn the same from the cosntructor else if no object is
+manually returned or a non object vlaue is returned then we return this from  the construtor.
+>In step 2, when we use the constructor , we assign the data member like name ,price,description and member
+fucntion like 'displayProduct'.
+*/
