e3046288f6e88860db4613518a696e37cef3634d
7nidhi <nidhi151@gmail.com>
2024-09-24 18:03:27 +0530

    Buidler Pattern Function and Destructuring

--- JavaScriptTopic/Module33OOPs/OOPs 3.js
+++ JavaScriptTopic/Module33OOPs/OOPs 3.js
@@ -42,3 +42,35 @@ class product{
 }
 const p=product.Builder.setName("Iphone").setPrice(150000).build();
 p.displayProduct();
+
+
+/*Object Destructuring
+Object is combination of key value pair and associated with one entity, but in destructuring,we can associate
+them to indivudul seperte variable.
+*/
+const Prod={name:"Nidhi",age:26,city:"varanasi"};
+const {name,age,city}=Prod;  // Object destructuring
+console.log(age);
+
+//Destruturing Object inside another object
+const purchasedProd={orderID:1,orderDate:"24/9/2024",...Prod};  //this ... spread operator is destructuring the one object inside another object0.
+console.log(purchasedProd);
+
+
+//To give new name to original key 
+const {name: firstname,age:year,city:dis}=Prod;
+console.log(firstname); 
+
+//unpack nested Object
+
+const Prod1={discount:500,name:"Iphone 16", price:160000,category:{name:"mobile",categoryID:123}};
+const {category :{categoryID}}=Prod1;
+console.log(categoryID);
+
+const clone={...Prod1};
+console.log(clone);   //make clone of Prod1 using  '... (spread)Operator'
+
+//Rest papmeter is oppsoite of spread operator. this pack objects in one entity.
+
+const {discount, ...productWithoutDiscount}=Prod1;  //this will remove discount and create new object without discount.
+console.log(productWithoutDiscount);     
