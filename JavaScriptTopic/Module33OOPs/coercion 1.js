725a9012ba853a484dcec9dc29f3bc0b76e06394
7nidhi <nidhi151@gmail.com>
2024-09-25 21:06:13 +0530

    Type Conversion

---
+++ JavaScriptTopic/Module33OOPs/coercion 1.js
@@ -0,0 +1,5 @@
+//Type conversion in JS
+//== operator(Abstract equality Comparision)-Check the type of operands, if not same then JS make the implisite conversion byself.
+
+
+//=== Operator(Strict Equality Comparision)- check the type of operands,if not same do not make changes.
