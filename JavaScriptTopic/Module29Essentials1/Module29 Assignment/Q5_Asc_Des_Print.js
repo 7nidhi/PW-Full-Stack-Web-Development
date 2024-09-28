088bae6db93fc7bbee99ec96e3f04961dd0adf51
7nidhi <nidhi151@gmail.com>
2024-09-28 21:12:40 +0530

    print num in Ascending and Descending order

---
+++ JavaScriptTopic/Module29Essentials1/Module29 Assignment/Q5_Asc_Des_Print.js
@@ -0,0 +1,13 @@
+//print the number from 1 to 25 in Ascending and Desscending order using while loop
+console.log("Ascending Order")
+let b=1;
+while(b<=25){
+    console.log(b);
+    b++;
+}
+console.log("Descending Order")
+let c=25
+while(c>=1){
+    console.log(c);
+    c--;
+}
