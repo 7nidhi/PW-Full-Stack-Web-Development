fa0d0ec24e1c2d23ba4590d5983d555a2f658afc
7nidhi <nidhi151@gmail.com>
2024-09-28 20:08:31 +0530

    Grade of Student

--- JavaScriptTopic/Module29Essentials1/Module29 Assignment/grade.js
+++ JavaScriptTopic/Module29Essentials1/Module29 Assignment/grade.js
@@ -1,5 +1,22 @@
-//Calculate the grade A,B,C,F
-let num=10;
+/* Calculate the grade of student based on their marks
+ if greater than 90 then A grade
+ if between 70 and 90 then B grade
+ if between 50 and 70 then C grade
+ Below 50 then F grade
+*/
+let num=45;
 if(num>90){
     console.log("A");
+}
+else if(num<=90 && num>70){
+    console.log("B");   
+}
+else if(num<=70 && num>=50){
+    console.log("C");
+}
+else if(num<50){
+    console.log("F");
+}
+else{
+    console.log("Enter a valid Number");
 }
