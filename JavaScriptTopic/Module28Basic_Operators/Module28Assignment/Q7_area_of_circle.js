4d0753eedc6a32eada1d11dfabe0a0d22e26a3a3
7nidhi <nidhi151@gmail.com>
2024-09-28 14:50:29 +0530

    Area of circle

---
+++ JavaScriptTopic/Module28Basic_Operators/Module28Assignment/Q7_area_of_circle.js
@@ -0,0 +1,5 @@
+//Calculate the area of circle given radius value of 10.
+let radius=10, pi=3.141592, areaOfCircle;
+areaOfCircle=pi*(radius**2);
+areaOfCircle=areaOfCircle.toFixed(3);
+console.log("Area of circle is :",areaOfCircle);
