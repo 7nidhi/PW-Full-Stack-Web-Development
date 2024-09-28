a88006d129390d94a7c0049c7d3eaa05d8f211ab
7nidhi <nidhi151@gmail.com>
2024-09-28 14:08:57 +0530

    Simple Interest

---
+++ JavaScriptTopic/Module28Basic_Operators/Module28Assignment/simpleInterest.js
@@ -0,0 +1,5 @@
+// Calculate the simple interest using this formula- simple interest=(principal*rate*time)/100
+let principal=1500,rate=1.5,time=3;
+let simple_interest;
+simple_interest=(principal*rate*time)/100;
+console.log(simple_interest);
