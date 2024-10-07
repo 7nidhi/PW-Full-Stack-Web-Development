2f272d4dd4633e47c478438907db8589fedb65b5
Nidhi Pal <nidhipal151@gmail.com>
2024-10-07 17:57:55 +0530

    Components in react

---
+++ React/Module38ComponentAndHooks/component.js
@@ -0,0 +1,9 @@
+//cpmponent used to DRY principle(Donot repeate yourself)
+// used for creating reusable UI and function
+/* 
+Moduling- used for create different componets in React
+>export module-
+    default export-used only once in module, we can change name of default export during import in another module.
+    named export- used export before the funtion that you want to export to another module.
+>import module-
+*/
