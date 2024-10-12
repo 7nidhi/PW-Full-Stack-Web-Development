8048bc1b7af8b1b895960c78603520e75539542a
Nidhi Pal <nidhipal151@gmail.com>
2024-10-12 14:11:25 +0530

    Calculate Sqaure using Arrow Fucntion

--- JavaScriptTopic/Module30Essentials2/Module30 Assignment/Q1_square_arrow_function.js
+++ JavaScriptTopic/Module30Essentials2/Module30 Assignment/Q1_square_arrow_function.js
@@ -1,1 +1,11 @@
-//Calculate square using arrow function
+/* Create an arrow function called square that takes a number as an argument and returns its square.
+ Use the arrow function to calculate the square of a given number and display the result. 
+ */
+const squareFunc = (num) => num * num;
+let number = 5;
+let result = squareFunc(number);
+
+console.log(`The square of ${number} is ${result}`);
+
+
+
