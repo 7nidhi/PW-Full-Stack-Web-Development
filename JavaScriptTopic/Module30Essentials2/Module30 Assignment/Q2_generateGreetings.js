d6fa3e6f45f24448d324e21a5a30c483175ba3a0
Nidhi Pal <nidhipal151@gmail.com>
2024-10-12 14:19:40 +0530

    Generate Greetings

--- JavaScriptTopic/Module30Essentials2/Module30 Assignment/Q2_generateGreetings.js
+++ JavaScriptTopic/Module30Essentials2/Module30 Assignment/Q2_generateGreetings.js
@@ -1,7 +1,12 @@
-//Generate the customized greetings
-/*jhgfrgr
-hvfgvuergu
-sfjbjfufbhr
-vhfvg*/
-let x=10;
-console.log(x);
+/*
+Create a JavaScript function called generateGreeting that takes a name as an argument and returns a
+personalized greeting message. Use this function to greet three different people.
+*/
+
+function generateGreeting(name) {
+    return `Hello, ${name}! Welcome to PW Skills Family.`;
+  }
+  console.log(generateGreeting("Nidhi"));
+  console.log(generateGreeting("Prafull"));
+  console.log(generateGreeting("Mahima"));
+  
