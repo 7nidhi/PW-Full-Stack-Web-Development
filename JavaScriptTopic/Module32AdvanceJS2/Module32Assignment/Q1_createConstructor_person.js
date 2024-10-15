349e6f183a5a36d9f31bf34bb57851235d616e2d
Nidhi Pal <nidhipal151@gmail.com>
2024-10-15 19:54:08 +0530

    Create Constructor for Person Information

---
+++ JavaScriptTopic/Module32AdvanceJS2/Module32Assignment/Q1_createConstructor_person.js
@@ -0,0 +1,5 @@
+/*
+Create an object constructor Person that takes name and age as parameters and initializes them. Also, add
+a method sayHello to greet the person.
+
+*/
--- JavaScriptTopic/Module32AdvanceJS2/Module32Assignment/assigment.js
+++
@@ -1,1 +0,0 @@
-/*This is advance js assignment*/
