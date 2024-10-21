925259661b4ca2eb7ee9141916b2476a1a65fd54
Nidhi Pal <nidhipal151@gmail.com>
2024-10-21 22:37:20 +0530

    Create Object calculator

---
+++ JavaScriptTopic/Module32AdvanceJS2/Module32Assignment/Q3_Create_ObjectCalculator.js
@@ -0,0 +1,4 @@
+/*
+Create an object Calculator with methods add, subtract, multiply, and divide. Demonstrate the usage of this
+within these methods such that method chaining of add, subtract, multiply and divide is possible.
+*/
