a9a3433b768de6ccd997c0871751cdc008342dd5
Nidhi Pal <nidhipal151@gmail.com>
2024-10-24 22:37:09 +0530

    Average of studnet marks

--- JavaScriptTopic/Module30Essentials2/avgOfStudentMarkArray.js
+++ JavaScriptTopic/Module30Essentials2/avgOfStudentMarkArray.js
@@ -5,7 +5,15 @@ for a given arrya student mark is 85,97,44,37,76,60.Find the average of studnet 
 let studentArr=[85,97,44,37,76,60];
 let sum=0
 for(let i=0;i<studentArr.length;i++){
-    sum=sum+studentArr[i];
+    sum+=studentArr[i];
 }
 let average=sum/studentArr.length;
 console.log(average);
+
+let marks=[85,97,44,37,76,60];
+let total=0;
+for (let val of marks){
+    total+=val;
+}
+let avg=total/marks.length;
+console.log(`average of Student marks is ${avg}`);
