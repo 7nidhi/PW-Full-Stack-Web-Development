3d0656e9d7df9fa987a262bf2ee950422198d57c
Nidhi Pal <nidhipal151@gmail.com>
2024-10-25 22:56:50 +0530

    Assignment 2

---
+++ JavaScriptTopic/Module32AdvanceJS2/Module32Assignment/Q2_CreateConstruct_employeeDetail.js
@@ -0,0 +1,4 @@
+/*
+Create a constructor Employee that inherits from the Person constructor of problem 1. Add an additional
+property designation and a method getDetails to display the employee details.
+*/
