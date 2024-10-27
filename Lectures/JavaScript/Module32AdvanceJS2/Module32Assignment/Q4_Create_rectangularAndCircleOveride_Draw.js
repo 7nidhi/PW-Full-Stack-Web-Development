e7c1cdd5be05b2348ddc87c51bdbed1d6f42e9d3
Nidhi Pal <nidhipal151@gmail.com>
2024-10-27 11:15:18 +0530

    Create Circle and rectangle Oveerride Base Class

--- Lectures/JavaScript/Module32AdvanceJS2/Module32Assignment/Q4_Create_rectangularAndCircleOveride_Draw.js
+++ Lectures/JavaScript/Module32AdvanceJS2/Module32Assignment/Q4_Create_rectangularAndCircleOveride_Draw.js
@@ -2,3 +2,31 @@
 Define a base class Shape with a method draw. Create two subclasses Circle and Rectangle that override
 the draw method. Demonstrate polymorphism using instances of these classes.
 */
+
+
+class Shape {
+    draw() {
+        console.log("Drawing a shape");
+    }
+}
+
+
+class Circle extends Shape {
+    draw() {
+        console.log("Drawing a circle");
+    }
+}
+
+
+class Rectangle extends Shape {
+    draw() {
+        console.log("Drawing a rectangle");
+    }
+}
+
+
+const shapes = [new Shape(), new Circle(), new Rectangle()];
+
+shapes.forEach(shape => {
+    shape.draw();
+});
