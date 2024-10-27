a04073cc98a6a34ae67d35736828e76bf3a9d49b
Nidhi Pal <nidhipal151@gmail.com>
2024-10-27 22:24:16 +0530

    Print Array using Map method

---
+++ Lectures/JavaScript/Module30Essentials2/map.js
@@ -0,0 +1,6 @@
+// Map method- Creates a new array, passed as argument in another funcion like forEach loop.
+let nums=[2,3,4,5,6,7];
+let newArr=nums.map((val) =>{
+    return val;
+ });
+ console.log(newArr);
