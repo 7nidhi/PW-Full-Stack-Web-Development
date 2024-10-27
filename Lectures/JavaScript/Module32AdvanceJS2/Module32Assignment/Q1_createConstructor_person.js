05b04791521a58516165e7c472ac2d7826276579
Nidhi Pal <nidhipal151@gmail.com>
2024-10-27 10:55:47 +0530

    Create Constructor and greet person

--- Lectures/JavaScript/Module32AdvanceJS2/Module32Assignment/Q1_createConstructor_person.js
+++ Lectures/JavaScript/Module32AdvanceJS2/Module32Assignment/Q1_createConstructor_person.js
@@ -3,3 +3,14 @@ Create an object constructor Person that takes name and age as parameters and in
 a method sayHello to greet the person.
 
 */
+function Person(name, age) {
+    this.name = name;
+    this.age = age;
+    
+    this.sayHello = function() {
+        console.log(`Hello, my name is ${this.name} and I am ${this.age} years old.`);
+    };
+}
+
+const person1 = new Person("Nidhi", 27);
+person1.sayHello(); 
