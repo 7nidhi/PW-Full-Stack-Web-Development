95a6a3144a3bdcbdb26e38be40a7cda13d2c8ce8
Nidhi Pal <nidhipal151@gmail.com>
2024-10-27 11:00:17 +0530

    Create person and inherit Employee details

--- Lectures/JavaScript/Module32AdvanceJS2/Module32Assignment/Q2_CreateConstruct_employeeDetail.js
+++ Lectures/JavaScript/Module32AdvanceJS2/Module32Assignment/Q2_CreateConstruct_employeeDetail.js
@@ -2,3 +2,27 @@
 Create a constructor Employee that inherits from the Person constructor of problem 1. Add an additional
 property designation and a method getDetails to display the employee details.
 */
+
+function Person(name, age) {
+    this.name = name;
+    this.age = age;
+    
+    this.sayHello = function() {
+        console.log(`Hello, my name is ${this.name} and I am ${this.age} years old.`);
+    };
+}
+
+function Employee(name, age, designation) {
+    
+    Person.call(this, name, age);
+    
+    this.designation = designation;
+    
+    this.getDetails = function() {
+        console.log(`Name: ${this.name}, Age: ${this.age}, Designation: ${this.designation}`);
+    };
+}
+
+const employee1 = new Employee("Nidhi", 27, "Software Engineer");
+employee1.sayHello();      
+employee1.getDetails();      
