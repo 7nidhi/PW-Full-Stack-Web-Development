2ebb55ddfece031ec10b88c0cf746a2665038e6d
Nidhi Pal <nidhipal151@gmail.com>
2024-10-27 11:07:07 +0530

    Create Calculator Using Chaining Method

--- Lectures/JavaScript/Module32AdvanceJS2/Module32Assignment/Q3_Create_ObjectCalculator.js
+++ Lectures/JavaScript/Module32AdvanceJS2/Module32Assignment/Q3_Create_ObjectCalculator.js
@@ -2,3 +2,40 @@
 Create an object Calculator with methods add, subtract, multiply, and divide. Demonstrate the usage of this
 within these methods such that method chaining of add, subtract, multiply and divide is possible.
 */
+
+const Calculator = {
+    value: 0,
+    
+    add: function(num) {
+        this.value += num;
+        return this;
+    },
+    
+    subtract: function(num) {
+        this.value -= num;
+        return this;
+    },
+    
+    multiply: function(num) {
+        this.value *= num;
+        return this;
+    },
+    
+    divide: function(num) {
+        if (num !== 0) {
+            this.value /= num;
+        } else {
+            console.log("Cannot divide by zero");
+        }
+        return this; 
+    },
+    
+    
+    getResult: function() {
+        return this.value;
+    },
+};
+
+
+const result = Calculator.add(10).subtract(2).multiply(3).divide(4).getResult();
+console.log(result); 
