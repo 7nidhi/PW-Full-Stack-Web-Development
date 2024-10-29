c9488ddde7d68425ec4b70e0d9b17a888bd07c3a
Nidhi Pal <nidhipal151@gmail.com>
2024-10-29 23:07:23 +0530

    DOM Manipultion

---
+++ Lectures/JavaScript/Module36DomManipulation/script.js
@@ -0,0 +1,14 @@
+let heading=document.getElementById("heading");
+console.dir(heading);
+
+let head=document.getElementsByClassName("head");
+console.dir(head);
+
+let paras=document.getElementsByTagName("p");
+console.dir(paras);
+
+let firstSelect=document.querySelector("p");  //select 1 element of matches p
+console.dir(firstSelect);
+
+let allSelect=document.querySelectorAll("p");  //select all elemnt of matches p
+console.dir(allSelect);
