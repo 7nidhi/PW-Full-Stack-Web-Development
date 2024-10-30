6b3ed9c6d2683c3efffa742c06f28d98bc641701
Nidhi Pal <nidhipal151@gmail.com>
2024-10-30 23:18:20 +0530

    Create Heading and Append Text

---
+++ Lectures/JavaScript/Module36DomManipulation/createHeadingAndApendText.js
@@ -0,0 +1,18 @@
+/*
+Create a h2 heading element with text "hello Java Script". and Append "From PW skills" to this text
+using JS.
+*/
+
+let h2=document.querySelector("h2");
+console.dir(h2.innerText);
+
+h2.innerText=h2.innerText+" from PW Skills.";
+
+
+/*
+Create 3 divs common class name "box".access them and add some unique text to each of them.
+*/
+
+let divs=document.querySelectorAll("box");
+console.dir(divs);
+
