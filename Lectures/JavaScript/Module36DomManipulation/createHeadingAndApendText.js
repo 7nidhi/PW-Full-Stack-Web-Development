05e3ecfff261aa27245fa2d30f14e6b20cbc12a2
Nidhi Pal <nidhipal151@gmail.com>
2024-11-02 22:21:21 +0530

    Dom Examples

--- Lectures/JavaScript/Module36DomManipulation/createHeadingAndApendText.js
+++ Lectures/JavaScript/Module36DomManipulation/createHeadingAndApendText.js
@@ -13,6 +13,14 @@ h2.innerText=h2.innerText+" from PW Skills.";
 Create 3 divs common class name "box".access them and add some unique text to each of them.
 */
 
-let divs=document.querySelectorAll("box");
-console.dir(divs);
+let divs=document.querySelectorAll(".box");
+console.dir(divs[0]);
+// divs[0].innerText="New value1";
+// divs[1].innerText="New value2";
+// divs[2].innerText="New value3";
 
+let idx=1;
+for(div of divs){
+    div.innerText=`new value ${idx}`;
+    idx++;
+}
