e08e4b51874b14235f1151486c7e893d1e31b2f0
Nidhi Pal <nidhipal151@gmail.com>
2024-11-03 23:29:29 +0530

    Add and delete element to node

--- Lectures/JavaScript/Module36DomManipulation/dom2.js
+++ Lectures/JavaScript/Module36DomManipulation/dom2.js
@@ -21,5 +21,32 @@ let divs=document.querySelector("div");
 console.log(divs);
 divs.style.backgroundColor="pink";
 divs.style.fontSize="30px";
+divs.innerText="HEllo Div";
 
+//Add new element, 1-create element 2-add element
 
+let newbtn=document.createElement("button");
+newbtn.innerText="Click Me";
+console.log(newbtn);
+
+//Add at the end of node (inside) -append()
+
+divs.append(newbtn);
+
+//Add at the start of node (inside)-prepend()
+divs.prepend(newbtn);
+
+//Add at the before of node (outside)
+divs.before(newbtn);
+
+//Add at the after of node (outside)
+divs.after(newbtn);
+
+let newHeading=document.createElement("h1");
+newHeading.innerHTML="<i> Hi, i am here. </i>";
+document.querySelector("body").prepend(newHeading);
+
+//delete  node
+
+let par1=document.querySelector("p");
+par1.remove();
