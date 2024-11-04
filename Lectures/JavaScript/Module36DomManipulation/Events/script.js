2a564977d039a4267814de507afb83a39a2a40df
Nidhi Pal <nidhipal151@gmail.com>
2024-11-04 23:02:57 +0530

    Event Listeners

--- Lectures/JavaScript/Module36DomManipulation/Events/script.js
+++ Lectures/JavaScript/Module36DomManipulation/Events/script.js
@@ -1,5 +1,19 @@
 let btn1=document.querySelector("#btn1");
-btn1.onclick= () =>{
-    console.log("Button was clicked.");
+// btn1.onclick= (evt) =>{                       //event object
+//     console.log("Button was clicked.");
+//     console.log(evt);
+//     console.log(evt.type);                    //check the event type =click
+//     console.log(evt.target);
+// };
 
-}
+btn1.addEventListener("click",(evt) =>{
+    console.log("Button was clicked");
+    console.log(evt);
+    console.log(evt.type);                    
+    console.log(evt.target);
+})
+
+let div=document.querySelector("div");
+div.onmouseover =()=>{
+    console.log("You are inside div");
+};
