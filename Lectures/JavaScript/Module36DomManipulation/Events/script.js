bab73430e4d4f8d78a1bd5abb112bdbab29b576c
Nidhi Pal <nidhipal151@gmail.com>
2024-11-04 23:22:48 +0530

    Add and remove Event Listeners

--- Lectures/JavaScript/Module36DomManipulation/Events/script.js
+++ Lectures/JavaScript/Module36DomManipulation/Events/script.js
@@ -7,11 +7,23 @@ let btn1=document.querySelector("#btn1");
 // };
 
 btn1.addEventListener("click",(evt) =>{
-    console.log("Button was clicked");
+    console.log("Button was clicked1");
     console.log(evt);
     console.log(evt.type);                    
     console.log(evt.target);
-})
+});
+
+btn1.addEventListener("click",(evt) =>{
+    console.log("Button was clicked2");
+});
+
+const handler3=(evt) =>{
+    console.log("Button was clicked3");
+}
+
+btn1.addEventListener("click",handler3);
+
+btn1.removeEventListener("click",handler3);          //Remove Event Listernes
 
 let div=document.querySelector("div");
 div.onmouseover =()=>{
