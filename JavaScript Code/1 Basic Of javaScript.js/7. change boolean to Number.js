d65f88f17f75426cc6ff3b12bcbc9522561951ae
Nidhi Pal <nidhipal151@gmail.com>
2024-11-05 11:33:21 +0530

    Convert a boolean to a number

---
+++ JavaScript Code/1 Basic Of javaScript.js/7. change boolean to Number.js
@@ -0,0 +1,11 @@
+//Convert a boolean to a number. 
+
+let a=true;
+let b=false;
+
+console.log(`the value of a and b before conversion:${a},${b}`);
+
+let y=Number(a);
+let z=Number(b);
+
+console.log(`the value of a and b after conversion:${y},${z}`);
