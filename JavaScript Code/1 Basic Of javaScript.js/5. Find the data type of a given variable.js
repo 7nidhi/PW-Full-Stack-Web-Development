6cbbcdb0e709315ac271b6b2e694320be97ec8df
Nidhi Pal <nidhipal151@gmail.com>
2024-11-05 11:32:38 +0530

    Find the data type of a given variable

---
+++ JavaScript Code/1 Basic Of javaScript.js/5. Find the data type of a given variable.js
@@ -0,0 +1,20 @@
+//Find the data type of a given variable.
+
+let a=10;
+let b="10";
+let c=true;
+let d=12334556887665543n;
+let e;
+let f=null;
+let g=123.45;
+
+
+console.log(`
+    The value of a:${a}, and type of a :${typeof(a)}, \n
+    The value of b:${b}, and type of a :${typeof(b)}, \n
+    The value of c:${c}, and type of a :${typeof(c)}, \n
+    The value of d:${d}, and type of a :${typeof(d)}, \n
+    The value of e:${e}, and type of a :${typeof(f)}, \n
+    The value of f:${f}, and type of a :${typeof(f)}, \n
+    The value of g:${g}, and type of a :${typeof(g)}, \n
+    `)
