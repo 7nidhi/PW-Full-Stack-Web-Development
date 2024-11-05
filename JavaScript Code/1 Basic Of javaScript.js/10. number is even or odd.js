52249d2cf355d136ed511b38751dc889c71c234b
Nidhi Pal <nidhipal151@gmail.com>
2024-11-05 11:34:28 +0530

    a number is even or odd

---
+++ JavaScript Code/1 Basic Of javaScript.js/10. number is even or odd.js
@@ -0,0 +1,13 @@
+//Find whether a number is even or odd.
+
+function CheckEvenOrOdd(num){
+    if(num%2==0){
+        console.log(`${num} is even.`);
+    }
+    else {
+        console.log(`${num} is odd.`);
+    }
+}
+
+CheckEvenOrOdd(15);
+CheckEvenOrOdd(10);
