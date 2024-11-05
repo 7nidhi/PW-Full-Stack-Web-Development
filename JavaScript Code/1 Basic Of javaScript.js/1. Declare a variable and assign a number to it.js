0c5bd521438f3a3b9461fb399e085093a93ecebc
Nidhi Pal <nidhipal151@gmail.com>
2024-11-05 11:30:42 +0530

    Declare a variable and assign number

---
+++ JavaScript Code/1 Basic Of javaScript.js/1. Declare a variable and assign a number to it.js
@@ -0,0 +1,7 @@
+// Declare a variable and assign number 
+
+let x=10;            //let can be reassigned but not redeclared in same scope. Block scope
+var y=20;            //var can be reassigned and redeclared. Global scope
+const z=30;          // const can not be reassigned and not update. Block scope.
+
+console.log(`let values is: ${x}, var value is: ${y}, const value is: ${z}`);
