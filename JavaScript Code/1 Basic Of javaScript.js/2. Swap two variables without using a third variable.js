f98cd643b8c2453837321fcc13b5d32a259cb01b
Nidhi Pal <nidhipal151@gmail.com>
2024-11-05 11:31:04 +0530

    Swap two number without third variable

---
+++ JavaScript Code/1 Basic Of javaScript.js/2. Swap two variables without using a third variable.js
@@ -0,0 +1,12 @@
+//Swap two number without third variable
+
+let a=10;
+let b=15;
+
+console.log(`Before swaping a=${a}, b=${b}`);
+
+a=a+b;
+b=a-b;
+a=a-b;
+
+console.log(`adter swaping a=${a}, b=${b}`);
