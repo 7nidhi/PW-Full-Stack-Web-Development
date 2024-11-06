e329549aebc27f6ccfeb8575331d3d0f3610849a
Nidhi Pal <nidhipal151@gmail.com>
2024-11-07 00:31:28 +0530

    Adding background js code

--- HandsOn/JavaScript/RockPaperScissor/script.js
+++ HandsOn/JavaScript/RockPaperScissor/script.js
@@ -1,1 +1,18 @@
-/*Rock Paper Scissor*////////////
+/*Rock Paper Scissor*/
+
+let userScore=0;
+let compScore=0;
+
+const choices=document.querySelectorAll(".choice");
+
+const playGame=(userChoice)=>{
+    console.log("user choice =",userChoice);
+
+}
+
+choices.forEach((choice)=>{
+    choice.addEventListener("click",()=>{
+        const userChoice=choice.getAttribute("id");
+        playGame(userChoice);
+    });
+});
