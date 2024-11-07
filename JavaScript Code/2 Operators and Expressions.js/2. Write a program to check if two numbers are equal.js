55b09ad684561cb2971d6e2442782a9d5ee9c50c
Nidhi Pal <nidhipal151@gmail.com>
2024-11-07 11:12:04 +0530

    Check two num are equal or not

---
+++ JavaScript Code/2 Operators and Expressions.js/2. Write a program to check if two numbers are equal.js
@@ -0,0 +1,12 @@
+// Write a program to check if two numbers are equal.
+
+function checkNumIsEqual(x,y){
+    if(x===y){
+        console.log("Number is equal");
+    }
+    else{
+        console.log("Number is not equal");
+    }
+}
+
+checkNumIsEqual(5,6);
