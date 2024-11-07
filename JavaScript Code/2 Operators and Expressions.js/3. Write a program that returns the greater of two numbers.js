70b65870483f766d54cff9b915dcc6c66f68de78
Nidhi Pal <nidhipal151@gmail.com>
2024-11-07 11:12:24 +0530

    Return greate num

---
+++ JavaScript Code/2 Operators and Expressions.js/3. Write a program that returns the greater of two numbers.js
@@ -0,0 +1,12 @@
+// Write a program that returns the greater of two numbers.
+
+function greater(x,y){
+    if(x>y){
+        console.log(`${x} is greater than ${y}`);
+    }
+    else{
+        console.log(`${y} is greater than ${x}`);
+    }
+}
+
+greater(4,6);
