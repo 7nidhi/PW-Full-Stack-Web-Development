9a710736e95843c5f2c226e0efb10ec56c876afa
Nidhi Pal <nidhipal151@gmail.com>
2024-11-07 11:11:29 +0530

    All arithmatic Operatiors

---
+++ JavaScript Code/2 Operators and Expressions.js/1. Perform all arithmetic operations on two numbers.js
@@ -0,0 +1,41 @@
+//Perform all arithmetic operations on two numbers.
+ function add(x,y){
+    return x+y;
+ }
+
+ function sub(x,y){
+    return x-y;
+ }
+
+ function multiply(x,y){
+    return x*y;
+ }
+
+ function div(x,y){
+    return x/y;
+ }
+
+ function modulus(x,y){
+    return x%y;
+ }
+
+ function increment(x){
+    return ++x;
+ }
+
+ function decrement(x){
+    return --x;
+ }
+ function exponent(x,y){
+    return x**y;
+ }
+
+
+ console.log(add(5,6));
+ console.log(sub(7,6));
+ console.log(multiply(10,7));
+ console.log(div(25,5));
+ console.log(increment(5));
+ console.log(decrement(5));
+ console.log(modulus(7,3));
+ console.log(exponent(5,3));
