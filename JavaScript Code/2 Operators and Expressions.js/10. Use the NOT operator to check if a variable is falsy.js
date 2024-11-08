78bead939d0f1d1aebace033db594835d70a3769
Nidhi Pal <nidhipal151@gmail.com>
2024-11-08 12:13:59 +0530

    check Falsy variable

---
+++ JavaScript Code/2 Operators and Expressions.js/10. Use the NOT operator to check if a variable is falsy.js
@@ -0,0 +1,9 @@
+//Use the NOT operator to check if a variable is falsy.
+
+let myVar = null;
+
+if (!myVar) {
+  console.log("The variable is falsy");
+} else {
+  console.log("The variable is truthy");
+}
