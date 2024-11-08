2dbfcb4fb4c77d339671332f70335671b48031e0
Nidhi Pal <nidhipal151@gmail.com>
2024-11-08 12:13:23 +0530

    Find Absolute Differnce

---
+++ JavaScript Code/2 Operators and Expressions.js/8. Find the absolute difference between two numbers.js
@@ -0,0 +1,8 @@
+//Find the absolute difference between two numbers.
+function getAbsDiff(x,y){
+    return Math.abs(x-y);
+}
+
+let a=-10;
+let b=7
+console.log(`Absolute differnece between ${a} and ${b} is ${getAbsDiff(a,b)}`);
