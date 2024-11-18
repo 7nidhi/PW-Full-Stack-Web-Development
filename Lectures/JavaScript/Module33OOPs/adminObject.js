28416a9fe8996f9847682f94b1e641e41eba5d0f
Nidhi Pal <nidhipal151@gmail.com>
2024-11-18 22:56:04 +0530

    Admin Object inherit from user

---
+++ Lectures/JavaScript/Module33OOPs/adminObject.js
@@ -0,0 +1,23 @@
+/*
+Create a new class called admin which inherit from user. Add n new method called editData to admin that allows
+it to edit website.
+*/
+let DATA="Secret Information";
+class user{
+    constructor(name,email){
+        this.name=name;
+        this.email=email;
+    }
+
+}
+
+class Admin extends user{
+    constructor(name,email){
+        super(name,email);
+    }
+    editData(){
+        DATA="Some new value";
+
+    }
+}
+let admin1=new Admin("Nidhi","nidhipal151@gmail.com");
