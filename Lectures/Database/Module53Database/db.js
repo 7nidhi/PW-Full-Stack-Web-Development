189e5113d91fb32b77cd3860c8473100570e787c
Nidhi Pal <nidhipal151@gmail.com>
2024-11-21 22:45:40 +0530

    Basic Info Of MongoDB

--- Lectures/Database/Module53Database/db.js
+++ Lectures/Database/Module53Database/db.js
@@ -1,1 +1,18 @@
-/*Database basic*/
+/*Database basic
+Database is software that helps to store and manipulate the data easily.
+Category-
+1-RDBMS (SQL Database,relational DB)
+2-No SQl(MongoDB, Redis,dynamicDB)
+
+
+NoSQl-(nit only SQL) store data none relational fashion. does not use table.
+Category-
+Graph database(Neo4j)
+Document database(MongoDB)-platform Independent,Open source
+key-value(Redis)
+
+NoSQL-
+How store data inrernally
+Json(Javasctipt Object Notation)
+Bson(Binary Json)
+*/
