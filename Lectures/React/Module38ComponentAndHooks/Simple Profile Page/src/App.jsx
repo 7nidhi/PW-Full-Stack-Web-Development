693fde1b3bf4cc592b5d379cfb09ac0231dedb59
Nidhi Pal <nidhipal151@gmail.com>
2025-01-02 10:23:03 +0530

    Single Page Profile

---
+++ Lectures/React/Module38ComponentAndHooks/Simple Profile Page/src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import PageProfile from './components/pageProfile'
+
+function App() {
+  
+
+  return (
+    <div>
+      <PageProfile/>
+    </div>
+    
+  )
+}
+
+export default App
---
+++ Lectures/React/Module38ComponentAndHooks/Simple Profile Page/src/components/PageProfile.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './pageProfile.css'
+
+const PageProfile = () => {
+  return (
+    <div>pageProfile</div>
+  )
+}
+
+export default PageProfile
