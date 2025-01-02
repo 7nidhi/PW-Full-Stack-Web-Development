020f46d1c8790faef9544a3752b00f334c657d99
Nidhi Pal <nidhipal151@gmail.com>
2025-01-02 10:28:05 +0530

    Changing Components

--- Lectures/React/Module38ComponentAndHooks/componentCreate/src/App.jsx
+++ Lectures/React/Module38ComponentAndHooks/componentCreate/src/App.jsx
@@ -1,13 +1,18 @@
 import UserCard from "./components/UserCard"
 import "./App.css"
+import cat from "./assets/cat.jpg"
+import cat2 from "./assets/cat2.jpg"
+import cat3 from "./assets/cat3.jpg"
+
+
 function App() {
   
 
   return (
     <div className="container">
-      <UserCard name="Cat1" desc="Catty1"/>
-      <UserCard name="Cat2" desc="Catty2"/>
-      <UserCard name="Cat3"/>
+      <UserCard name="Kitty" desc="Catty1" image={cat}/>
+      <UserCard name="Chitty" desc="Catty2" image={cat2}/>
+      <UserCard name="Missy" desc="Catty3" image={cat3}/>
     </div>
   )
 }
--- Lectures/React/Module38ComponentAndHooks/componentCreate/src/components/UserCard.jsx
+++ Lectures/React/Module38ComponentAndHooks/componentCreate/src/components/UserCard.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import cat from '../assets/cat.jpg'
 import "./UserCard.css"
+
+
+
+
 const UserCard = (props) => {
   return (
     <div className='user-container'>
         <p id='user-card'>{props.name}</p>
-        <img id='user-image' src={cat} alt='Cat'></img>
+        <img id='user-image' src={props.image} alt={props.name}></img>
         <p id='user-desc'>{props.desc}</p>
     </div>
   )
