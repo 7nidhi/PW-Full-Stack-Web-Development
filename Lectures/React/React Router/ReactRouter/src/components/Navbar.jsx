2ef941b74b301ae8775bab08eece3129d58d4f0c
Nidhi Pal <nidhipal151@gmail.com>
2025-01-08 11:36:24 +0530

    Adding NavLink tag

--- Lectures/React/React Router/ReactRouter/src/components/Navbar.jsx
+++ Lectures/React/React Router/ReactRouter/src/components/Navbar.jsx
@@ -2,20 +2,23 @@ import React from 'react'
 import './Navbar.css'
 import About from './About'
 import Dashboard from './Dashboard'
-import { Link } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
 
 const Navbar = () => {
   return (
     <div>
     <ul>
         <li>
-            <Link to='/'>Home</Link>
+            <NavLink to='/' className={({isActive})=>isActive?"active-link":" "}>
+            Home</NavLink>
         </li>
         <li>
-            <Link to='/about'>About</Link>
+            <NavLink to='/about' className={({isActive})=>isActive?"active-link":" "}>
+            About </NavLink>
         </li>
         <li>
-            <Link to='/dashboard'>Dashboard</Link>
+            <NavLink to='/dashboard' className={({isActive})=>isActive?"active-link":" "}>
+            Dashboard</NavLink>
         </li>
         
     </ul>
