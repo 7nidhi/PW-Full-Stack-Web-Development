9d55aa443576c93f5770c9870501707f726bb7ba
Nidhi Pal <nidhipal151@gmail.com>
2025-01-10 11:34:51 +0530

    Create React Form using useForm Hook

---
+++ Lectures/React/ReactForm/reactForm/src/App.jsx
@@ -0,0 +1,21 @@
+
+import './App.css'
+
+function App() {
+   const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm()
+
+
+  return (
+   
+    <div>
+      Nidhi
+    </div>
+  )
+}
+
+export default App
