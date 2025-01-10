b179f3770c399c5879be63fa4e19d0dd3c11973e
Nidhi Pal <nidhipal151@gmail.com>
2025-01-10 17:57:40 +0530

    form validation

--- Lectures/React/ReactForm/reactForm/src/App.jsx
+++ Lectures/React/ReactForm/reactForm/src/App.jsx
@@ -1,4 +1,4 @@
-
+import { useForm} from "react-hook-form"
 import './App.css'
 
 function App() {
@@ -8,13 +8,41 @@ function App() {
     watch,
     formState: { errors },
   } = useForm()
-
+function onSubmit(data){
+  console.log("Submitting the form,",data);
+}
 
   return (
-   
-    <div>
-      Nidhi
-    </div>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div>
+        <label>First Name:</label>
+        <input
+         className={errors.firstName ?'input-error':""}
+        {...register("firstName",
+          { 
+          required: true, 
+          minLength:{value:3,message:'Min lenght should be atleast 3'},
+          maxLength:20,
+          }
+          )}/>
+          {errors.firstName && <p className="error-msg">{errors.firstName.message}</p>}
+        
+      </div>
+      <div>
+        <label>Middle Name:</label>
+        <input {...register("middleName",{ required: true, maxLength: 20,minLength:3 })}/>
+      </div>
+      <div>
+        <label>Last Name:</label>
+        <input {...register("lastName", 
+        { pattern:{
+          value:/^[A-Za-z]+$/i, 
+          message:"Last name is not as per rule",
+        } })}/>
+        {errors.lastName && <p className="error-msg">{errors.lastName.message}</p>}
+      </div>
+      <input type='submit'/>
+    </form>
   )
 }
 
