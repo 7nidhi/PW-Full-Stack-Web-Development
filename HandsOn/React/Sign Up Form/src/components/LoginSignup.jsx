0d44d0a7a442c2123778f3bec190982e1ad079d2
Nidhi Pal <nidhipal151@gmail.com>
2025-01-21 12:13:16 +0530

    Adding action in Form

--- HandsOn/React/Sign Up Form/src/components/LoginSignup.jsx
+++ HandsOn/React/Sign Up Form/src/components/LoginSignup.jsx
@@ -1,31 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './LoginSignup.css'
-import  user_icon from '../assets'
-import password_icon from '../assets'
-import email_icon from '../assets'
+import  user_icon from '../assets/person.png'
+import password_icon from '../assets/password.png'
+import email_icon from '../assets/email.png'
 
 
 const LoginSignup = () => {
+    const [action,setAction]=useState("Login");
   return (
     <div className='container'>
     <div className='header'>
-    <div className='text'>Sign Up</div>
+    <div className='text'>{action}</div>
     <div className='under-line'></div>
     </div>
     <div className='inputs'>
-        <div className='input'>
+    {action==="Login"?<div></div>:<div className='input'>
             <img src={user_icon} alt=''></img>
-            <input type='text'></input>
-        </div>
+            <input type='text' placeholder='Name'></input>
+        </div>}
+        
         <div className='input'>
             <img src={email_icon} alt=''></img>
-            <input type='email'></input>
+            <input type='email' placeholder='Email'></input>
         </div>
         <div className='input'>
             <img src={password_icon} alt=''></img>
-            <input type='password'></input>
+            <input type='password' placeholder='Password'></input>
         </div>
-
+    </div>
+    {action==="Login"?<div></div>:
+    <div className='forget-password'>Forget password ? <span>Click Here!</span></div>}
+    
+    <div className='submit-container'>
+        <div className={action==="Login"?"submit gray":"submit"} onClick={()=>{setAction("Sign Up")}}>Sign Up</div>
+        <div className={action==="Sign Up"?"submit gray":"submit"} onClick={()=>{setAction("Login")}}>Login</div>
     </div>
 
 
@@ -33,4 +41,4 @@ const LoginSignup = () => {
   )
 }
 
-export default LoginSignup
+export default LoginSignup;
