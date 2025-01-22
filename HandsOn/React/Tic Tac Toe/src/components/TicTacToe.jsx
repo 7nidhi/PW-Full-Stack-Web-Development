771cf3795c3589fb4e8f391db03570774aef2c3c
Nidhi Pal <nidhipal151@gmail.com>
2025-01-22 23:18:50 +0530

    Adding Style

--- HandsOn/React/Tic Tac Toe/src/components/TicTacToe.jsx
+++ HandsOn/React/Tic Tac Toe/src/components/TicTacToe.jsx
@@ -7,8 +7,23 @@ const TicTacToe = () => {
   return (
     <div className='container'>
     <h1 className='tittle'>Tic Tac Toe By <span>React</span></h1>
-    <div>
-        Board
+    <div className='board'>
+    <div className='row1'>
+        <div className='boxes'></div>
+        <div className='boxes'></div>
+        <div className='boxes'></div>
+    </div>
+    <div className='row2'>
+        <div className='boxes'></div>
+        <div className='boxes'></div>
+        <div className='boxes'></div>
+    </div>
+    <div className='row3'>
+        <div className='boxes'></div>
+        <div className='boxes'></div>
+        <div className='boxes'></div>
+    </div>
+       
     </div>
     <button className='reset'>Reset</button>
 
