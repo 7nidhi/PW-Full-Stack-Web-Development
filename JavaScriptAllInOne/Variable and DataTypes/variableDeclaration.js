9a898bb374d5bb46ceefa1ca924c88a1afd94fb7
Nidhi Pal <nidhipal151@gmail.com>
2025-01-25 11:22:20 +0530

    Variable Declaration

---
+++ JavaScriptAllInOne/Variable and DataTypes/variableDeclaration.js
@@ -0,0 +1,23 @@
+// let - variable can not be re-declared but can be updated. This is block scope variable.
+
+let fullname="Nidhi Pal";
+let age=28;
+console.log(fullname);
+console.log(age);
+
+
+//var -variable can re-declared and updated. This is a global scope variable.
+
+/*
+var a=25;
+var a=26;
+var a=27;
+console.log(a);       Output=27;
+
+*/
+
+
+// const - variable can not be re-declare or updated. This is block varibale.
+ const PI=3.14;
+  //PI=3.15;   can not reassign
+ console.log(PI);
