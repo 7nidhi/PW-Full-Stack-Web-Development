6ca6ccdf87b66df2750c04c42a2730fa3bd028e7
Nidhi Pal <nidhipal151@gmail.com>
2025-01-27 11:44:19 +0530

    Logical Operators

---
+++ JavaScriptAllInOne/Operators/logicalOperators.js
@@ -0,0 +1,11 @@
+// AND && , OR || , NOT ! ;
+let a=6;
+let b=4;
+
+let condi1=a>b;
+let condi2= a===7;
+let condi3= b<a;
+
+console.log("condi && condi2 is ",condi1 && condi2);
+console.log("condi || condi2 is ",condi1 || condi2);
+console.log("!condi3 is ",!condi3);
