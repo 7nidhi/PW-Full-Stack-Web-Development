c764db70fa9bfa7f089e6aacf3c2846b4e85fe70
Nidhi Pal <nidhipal151@gmail.com>
2025-01-28 11:13:39 +0530

    else if Statements

---
+++ JavaScriptAllInOne/Conditional Statements/else_if_statement.js
@@ -0,0 +1,13 @@
+let mode="Silver";
+let color="Black";
+
+if(mode==="Dark"){
+    color="Black";
+}else if(mode==="Pink"){
+    color="Pink";
+}else if(mode==="Blue"){
+    color="Blue";
+}else{
+    color="White"
+}
+console.log(color);
--- JavaScriptAllInOne/Conditional Statements/if_statement.js
+++ JavaScriptAllInOne/Conditional Statements/if_statement.js
@@ -10,3 +10,12 @@ else{
 }
 
 console.log(color);
+
+
+let number=13;
+if(number %2==0){
+    console.log(number, "is even");
+}
+else{
+    console.log(number,"is Odd");
+}
