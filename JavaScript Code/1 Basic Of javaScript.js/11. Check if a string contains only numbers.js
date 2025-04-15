4425b27d287e6dbf035894d591cae595083d76b5
Nidhi Pal <nidhipal151@gmail.com>
2025-04-15 12:14:04 +0530

    delete file

--- JavaScript Code/1 Basic Of javaScript.js/11. Check if a string contains only numbers.js
+++
@@ -1,9 +0,0 @@
-//Check if a string contains only numbers.
-function isNumber(str){
-    return !isNaN(str) && str.trim()!=="";
-}
-
-let a="23456";
-let b="2345ds";
-console.log(`${a} contains only numbers = ${isNumber(a)}`);
-console.log(`${b} constains only numbers = ${isNumber(b)}`);
--- JavaScript Code/1 Basic Of javaScript.js/15. Convert a number to a fixed decimal format.js
+++
@@ -1,9 +0,0 @@
-//Convert a number to a fixed decimal format. 
-
-function fixedDecimal(num,decimal_place){
-    console.log(`${num},${decimal_place}: => ${num.toFixed(decimal_place)}`);
-
-}
-
-fixedDecimal(123.345,2);
-fixedDecimal(12345.22,3);
